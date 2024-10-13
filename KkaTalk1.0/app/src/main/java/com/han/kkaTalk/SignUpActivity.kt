fec18f86625badf2f36574ebb49797a27adf59a4
SeungHwan Lee <asdf26532@gmail.com>
2024-10-13 19:56:34 +0900

    signup page

--- KkaTalk1.0/app/src/main/java/com/han/kkaTalk/SignUpActivity.kt
+++ KkaTalk1.0/app/src/main/java/com/han/kkaTalk/SignUpActivity.kt
@@ -1,20 +1,65 @@
 package com.han.kkaTalk
 
+import android.content.Intent
 import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.database.DatabaseReference
+import com.google.firebase.database.ktx.database
+import com.google.firebase.ktx.Firebase
+import com.han.kkaTalk.databinding.ActivitySignUpBinding
 
 class SignUpActivity : AppCompatActivity() {
+
+    lateinit var binding: ActivitySignUpBinding
+
+    lateinit var mAuth: FirebaseAuth
+
+    private lateinit var mDbRef: DatabaseReference
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
-        setContentView(R.layout.activity_sign_up)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+        binding = ActivitySignUpBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        // 인증 초기화
+        mAuth = Firebase.auth
+
+        // DB 초기화
+        mDbRef = Firebase.database.reference
+
+        binding.btnSignUp.setOnClickListener {
+            val name = binding.edtName.text.toString().trim()
+            val email = binding.edtEmail.text.toString().trim()
+            val password = binding.edtPassword.text.toString().trim()
+            val nick = binding.edtNick.text.toString().trim()
+
+            signUp(name, email, password, nick)
+
         }
     }
+
+    // 회원 가입 기능
+    private fun signUp(name:String, email:String, password:String, nick:String) {
+
+        mAuth.createUserWithEmailAndPassword(email, password)
+            .addOnCompleteListener(this) { task ->
+                if (task.isSuccessful) {
+                    Toast.makeText(this, "회원가입 성공",Toast.LENGTH_LONG).show()
+                    val intent: Intent = Intent(this@SignUpActivity, MainActivity::class.java)
+                    startActivity(intent)
+                    addUserToDatabase(name, email, mAuth.currentUser?.uid!!, nick)
+                } else {
+                    Toast.makeText(this, "회원가입 실패",Toast.LENGTH_LONG).show()
+                }
+            }
+
+    }
+
+    private fun addUserToDatabase(name:String, email:String, uId: String, nick: String) {
+        mDbRef.child("user").child(uId).setValue(User(name, email, uId, nick))
+    }
+
 }
