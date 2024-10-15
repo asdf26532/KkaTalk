da2b49b48e9c96c07c6dfdcb37f71e9c83c1b290
SeungHwan Lee <asdf26532@gmail.com>
2024-10-15 17:09:12 +0900

    frament setting

--- KkaTalk1.0/app/src/main/java/com/han/kkaTalk/MainActivity.kt
+++ KkaTalk1.0/app/src/main/java/com/han/kkaTalk/MainActivity.kt
@@ -2,11 +2,47 @@ package com.han.kkaTalk
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentTransaction
+import com.han.kkaTalk.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding : ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        replaceFragment(Home())
+
+        setContentView(binding.root)
+
+        binding.bottomNav.setOnItemSelectedListener {
+            when(it.itemId) {
+                R.id.home -> replaceFragment(Home())
+                R.id.chatting -> replaceFragment(Chatting())
+                R.id.setting -> replaceFragment(Setting())
+
+                else -> {
+
+                }
+            }
+
+            true
+        }
+
 
     }
+
+    private fun replaceFragment(fragment: Fragment) {
+
+        val fragmentManager = supportFragmentManager
+        val framentTransaction = fragmentManager.beginTransaction()
+        framentTransaction.replace(R.id.frame_layout,fragment)
+        framentTransaction.commit()
+
+
+    }
+
+
 }
