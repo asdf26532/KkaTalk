a59a18654ec67af8586904225870b5331d3ebcc8
SeungHwan Lee <asdf26532@gmail.com>
2024-10-17 10:23:27 +0900

    chatting

--- KkaTalk1.0/app/src/main/java/com/han/kkaTalk/ChatActivity.kt
+++ KkaTalk1.0/app/src/main/java/com/han/kkaTalk/ChatActivity.kt
@@ -1,20 +1,105 @@
 package com.han.kkaTalk
 
 import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.database.DataSnapshot
+import com.google.firebase.database.DatabaseError
+import com.google.firebase.database.DatabaseReference
+import com.google.firebase.database.FirebaseDatabase
+import com.google.firebase.database.ValueEventListener
+import com.han.kkaTalk.databinding.ActivityChatBinding
+
 
 class ChatActivity : AppCompatActivity() {
+
+    private lateinit var receiverName: String
+    private lateinit var receiverUid: String
+
+    private lateinit var binding: ActivityChatBinding
+
+    lateinit var mAuth: FirebaseAuth
+    lateinit var mDbRef: DatabaseReference
+
+    private lateinit var receiverRoom: String
+    private lateinit var senderRoom: String
+
+    private lateinit var messageList: ArrayList<Message>
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
-        setContentView(R.layout.activity_chat)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+        binding = ActivityChatBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        messageList = ArrayList()
+        val messageAdapter: MessageAdapter = MessageAdapter(this, messageList)
+
+        // RecyclerView
+        binding.rvChat.layoutManager = LinearLayoutManager(this)
+        binding.rvChat.adapter = messageAdapter
+
+        // 넘어온 데이터 변수에 담기
+        receiverName = intent.getStringExtra("name").toString()
+        receiverUid = intent.getStringExtra("uId").toString()
+
+        mAuth = FirebaseAuth.getInstance()
+        mDbRef = FirebaseDatabase.getInstance().reference
+
+        // 접속자 Uid
+        val senderUid = mAuth.currentUser?.uid
+
+        senderRoom = receiverUid + senderUid
+        receiverRoom = senderUid + receiverUid
+
+
+        // 액션바에 상대방 이름 보이기
+        supportActionBar?.title = receiverName
+
+        binding.btnSend.setOnClickListener{
+
+            val message = binding.edtMessage.text.toString()
+            val messageObject = Message(message, senderUid)
+
+            // 데이터 저장
+            mDbRef.child("chats").child(senderRoom).child("message").push()
+                .setValue(messageObject).addOnSuccessListener {
+                    // 저장 성공시
+                    mDbRef.child("chats").child(receiverRoom).child("message").push()
+                        .setValue(messageObject)
+                }
+            // 입력 부분 초기화
+            binding.edtMessage.setText("")
         }
+
+        // 메시지 가져오기
+        mDbRef.child("chats").child(senderRoom).child("message")
+            .addValueEventListener(object: ValueEventListener{
+
+                override fun onDataChange(snapshot: DataSnapshot) {
+                    messageList.clear()
+
+                    for(postSnapshot in snapshot.children) {
+                        val message = postSnapshot.getValue(Message::class.java)
+                        if (message != null) {
+                            messageList.add(message)
+                        }
+                    }
+                    // 적용
+                    messageAdapter.notifyItemInserted(messageList.size - 1)
+                    binding.rvChat.scrollToPosition(messageList.size - 1) // 스크롤을 최신 메시지로 이동
+
+                }
+
+                override fun onCancelled(error: DatabaseError) {
+                    TODO("Not yet implemented")
+                }
+
+
+            })
+
+
+
     }
 }
