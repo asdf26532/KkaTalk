552d850de7c40db79ed0e6364b0c223e2057aeca
SeungHwan Lee <asdf26532@gmail.com>
2024-10-29 16:35:07 +0900

    profile UI

--- KkaTalk1.0/app/src/main/java/com/han/kkaTalk/SettingFragment.kt
+++ KkaTalk1.0/app/src/main/java/com/han/kkaTalk/SettingFragment.kt
@@ -7,11 +7,16 @@ import android.view.View
 import android.view.ViewGroup
 import android.widget.Button
 import android.widget.EditText
+import android.widget.ImageView
 import android.widget.TextView
 import androidx.fragment.app.Fragment
+import com.bumptech.glide.Glide
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.database.DatabaseReference
 import com.google.firebase.database.FirebaseDatabase
+import com.google.firebase.ktx.Firebase
+import com.google.firebase.storage.ktx.storage
+import com.kakao.sdk.user.model.Profile
 
 class SettingFragment : Fragment() {
 
@@ -20,6 +25,8 @@ class SettingFragment : Fragment() {
     private lateinit var edtNewNick: EditText
     private lateinit var btnSaveNewNick: Button
     private lateinit var btnLogout: Button
+    private lateinit var btnChangeProfile: Button
+    private lateinit var ivProfile: ImageView
 
     private lateinit var mAuth: FirebaseAuth
     private lateinit var mDbRef: DatabaseReference
@@ -41,8 +48,11 @@ class SettingFragment : Fragment() {
         edtNewNick = view.findViewById(R.id.edt_new_nick)
         btnSaveNewNick = view.findViewById(R.id.btn_save_new_nick)
         btnLogout = view.findViewById(R.id.btn_logout)
+        btnChangeProfile = view.findViewById(R.id.btn_change_profile)
+        ivProfile = view.findViewById(R.id.iv_profile)
 
-
+        // 현재 프로필 사진 로드
+        loadCurrentProfile()
 
         // 현재 닉네임 로드
         loadCurrentNick()
@@ -72,6 +82,32 @@ class SettingFragment : Fragment() {
         }
 
         return view
+    }
+
+    private fun loadCurrentProfile() {
+        val userId = mAuth.currentUser?.uid.toString()
+
+        if (userId.isNotEmpty()) {
+            mDbRef.child("user").child(userId).get().addOnSuccessListener { snapshot ->
+                val currentUser = snapshot.getValue(User::class.java)
+                val profileImageUrl = currentUser?.profileImageUrl ?: "gs://kkatalk-cf3fd.appspot.com/profile_default.png"
+
+                // Firebase Storage 참조 생성
+                val storageRef = Firebase.storage.getReferenceFromUrl(profileImageUrl)
+
+                // Firebase Storage에서 이미지 URL 다운로드
+                storageRef.downloadUrl.addOnSuccessListener { uri ->
+                    // Glide를 사용해 ImageView에 이미지 로드
+                    Glide.with(this@SettingFragment)
+                        .load(uri)
+                        .placeholder(R.drawable.profile_default) // 기본 이미지 설정
+                        .into(ivProfile)
+                }.addOnFailureListener {
+                    // 실패 시 기본 이미지 로드
+                    ivProfile.setImageResource(R.drawable.profile_default)
+                }
+            }
+        }
     }
 
     private fun loadCurrentNick() {
