71cc3dd9a0f99bd31b1064ea97735489cc6a8500
SeungHwan Lee <asdf26532@gmail.com>
2024-11-25 20:12:40 +0900

    load message

--- KkaTalk1.0/app/src/main/java/com/han/kkaTalk/ChatActivity.kt
+++ KkaTalk1.0/app/src/main/java/com/han/kkaTalk/ChatActivity.kt
@@ -5,10 +5,10 @@ import android.content.Intent
 import android.os.Bundle
 import android.util.Log
 import android.view.MenuItem
-import androidx.activity.OnBackPressedCallback
 import androidx.appcompat.app.AppCompatActivity
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.database.ChildEventListener
 import com.google.firebase.database.DataSnapshot
 import com.google.firebase.database.DatabaseError
 import com.google.firebase.database.DatabaseReference
@@ -25,162 +25,139 @@ class ChatActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityChatBinding
 
-    lateinit var mAuth: FirebaseAuth
-    lateinit var mDbRef: DatabaseReference
+    private lateinit var mAuth: FirebaseAuth
+    private lateinit var mDbRef: DatabaseReference
 
     private lateinit var receiverRoom: String
     private lateinit var senderRoom: String
 
     private var profileImageUrl: String? = null
     private lateinit var messageList: ArrayList<Message>
+    private lateinit var messageAdapter: MessageAdapter
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityChatBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        // 넘어온 데이터 변수에 담기
+        // 데이터 가져오기
         receiverNick = intent.getStringExtra("nick").toString()
         receiverUid = intent.getStringExtra("uId").toString()
         profileImageUrl = intent.getStringExtra("profileImageUrl") ?: ""
 
-        // 데이터가 제대로 넘어오는지 로그 확인
-        Log.d("ChatActivity", "Receiver Name: $receiverNick")
-        Log.d("ChatActivity", "Receiver UID: $receiverUid")
-        Log.d("ChatActivity", "Profile Image URL: $profileImageUrl")
-
-        messageList = ArrayList()
-        val messageAdapter = MessageAdapter(this, messageList, profileImageUrl, receiverNick)
-
-        // RecyclerView
-        binding.rvChat.layoutManager = LinearLayoutManager(this)
-        binding.rvChat.adapter = messageAdapter
-
-        // Firebase 설정
         mAuth = FirebaseAuth.getInstance()
         mDbRef = FirebaseDatabase.getInstance().reference
 
-        // 접속자 Uid
         val senderUid = mAuth.currentUser?.uid
-
         senderRoom = receiverUid + senderUid
         receiverRoom = senderUid + receiverUid
 
+        messageList = ArrayList()
+        messageAdapter = MessageAdapter(this, messageList, profileImageUrl, receiverNick)
 
-        // 액션바에 상대방 이름 보이기
+        // RecyclerView 초기화
+        binding.rvChat.layoutManager = LinearLayoutManager(this)
+        binding.rvChat.adapter = messageAdapter
+
+        // Firebase에서 메시지 로드
+        loadMessages()
+
+        // 액션바 설정
         supportActionBar?.title = receiverNick
-        supportActionBar?.setDisplayHomeAsUpEnabled(true) // 화살표 버튼 추가
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
 
+        // 메시지 전송
         binding.btnSend.setOnClickListener {
-            val message = binding.edtMessage.text.toString()
-            val timeStamp = System.currentTimeMillis()
-            val mread = false
-
-            val messageObject = Message(message, senderUid, receiverUid, timeStamp, mread)
-
-            // 데이터 저장
-            mDbRef.child("chats").child(senderRoom).child("message").push()
-                .setValue(messageObject).addOnSuccessListener {
-                    // 저장 성공시
-                    mDbRef.child("chats").child(receiverRoom).child("message").push()
-                        .setValue(messageObject)
-                }
-
-            // 입력 부분 초기화
-            binding.edtMessage.setText("")
+            sendMessage(senderUid)
         }
+    }
 
-        // 메시지 가져오기
+    private fun loadMessages() {
+        // RecyclerView 데이터 초기화
+        messageList.clear()
+
         mDbRef.child("chats").child(senderRoom).child("message")
-            .addValueEventListener(object: ValueEventListener{
-                override fun onDataChange(snapshot: DataSnapshot) {
-                    val tempMessages = ArrayList<Message>()
-
-                    for(postSnapshot in snapshot.children) {
-                        val message = postSnapshot.getValue(Message::class.java)
-                        if (message != null && !messageList.contains(message)) {
-                            tempMessages.add(message)
-                        }
-                    }
-                    if (tempMessages.isNotEmpty()) {
-                        messageList.clear()
-                        messageList.addAll(tempMessages)
+            .addChildEventListener(object : ChildEventListener {
+                override fun onChildAdded(snapshot: DataSnapshot, previousChildName: String?) {
+                    val message = snapshot.getValue(Message::class.java)
+                    if (message != null) {
+                        messageList.add(message)
                         binding.rvChat.post {
-                            messageAdapter.notifyDataSetChanged()
+                            // RecyclerView 갱신 및 스크롤
+                            messageAdapter.notifyItemInserted(messageList.size - 1)
                             binding.rvChat.scrollToPosition(messageList.size - 1)
                         }
                     }
+                }
 
+                override fun onChildChanged(snapshot: DataSnapshot, previousChildName: String?) {
+                    // 메시지 변경 처리 (필요 시 구현)
+                }
+
+                override fun onChildRemoved(snapshot: DataSnapshot) {
+                    // 메시지 삭제 처리 (필요 시 구현)
+                }
+
+                override fun onChildMoved(snapshot: DataSnapshot, previousChildName: String?) {
+                    // 메시지 이동 처리 (필요 시 구현)
                 }
 
                 override fun onCancelled(error: DatabaseError) {
-                    Log.e("DatabaseError", "Database error: $error")
+                    Log.e("DatabaseError", "Error: ${error.message}")
                 }
             })
     }
 
+    private fun sendMessage(senderUid: String?) {
+        val message = binding.edtMessage.text.toString()
+        val timeStamp = System.currentTimeMillis()
+        val mread = false
+
+        val messageObject = Message(message, senderUid, receiverUid, timeStamp, mread)
+
+        // 메시지 저장
+        mDbRef.child("chats").child(senderRoom).child("message").push()
+            .setValue(messageObject).addOnSuccessListener {
+                mDbRef.child("chats").child(receiverRoom).child("message").push()
+                    .setValue(messageObject)
+            }
+
+        // 입력 필드 초기화
+        binding.edtMessage.setText("")
+    }
+
+    override fun onResume() {
+        super.onResume()
+        // 메시지 읽음 처리
+        markMessagesAsRead(senderRoom, receiverRoom)
+    }
+
     private fun markMessagesAsRead(senderRoom: String, receiverRoom: String) {
         val senderMessagesRef = mDbRef.child("chats").child(senderRoom).child("message")
-        val receiverMessagesRef = mDbRef.child("chats").child(receiverRoom).child("message")
 
-        // Sender Room 업데이트
         senderMessagesRef.addListenerForSingleValueEvent(object : ValueEventListener {
             override fun onDataChange(snapshot: DataSnapshot) {
                 for (messageSnapshot in snapshot.children) {
                     val message = messageSnapshot.getValue(Message::class.java)
-                    if (message != null && (message.mread == false) && message.sendId != FirebaseAuth.getInstance().currentUser?.uid) {
-                        // Sender Room 업데이트
+                    if (message != null && message.sendId != mAuth.currentUser?.uid) {
                         messageSnapshot.ref.child("mread").setValue(true)
-
-                        // Receiver Room 업데이트
-                        message.timestamp?.let { timestamp ->  // timestamp가 null이 아닌 경우만 실행
-                            receiverMessagesRef.orderByChild("timestamp")
-                                .equalTo(timestamp.toDouble())
-                                .addListenerForSingleValueEvent(object : ValueEventListener {
-                                    override fun onDataChange(receiverSnapshot: DataSnapshot) {
-                                        for (receiverMessageSnapshot in receiverSnapshot.children) {
-                                            receiverMessageSnapshot.ref.child("mread")
-                                                .setValue(true)
-                                        }
-                                    }
-
-                                    override fun onCancelled(error: DatabaseError) {
-                                        Log.e("markMessagesAsRead", "Database error: $error")
-                                    }
-                                })
-                        }
                     }
                 }
             }
 
             override fun onCancelled(error: DatabaseError) {
-                Log.e("markMessagesAsRead", "Database error: $error")
+                Log.e("markMessagesAsRead", "Error: ${error.message}")
             }
         })
     }
 
-
-
-    override fun onResume() {
-        super.onResume()
-        // 메시지 읽음 상태 업데이트
-        markMessagesAsRead(senderRoom, receiverRoom)
-    }
-
-    // 뒤로 가기 버튼 동작 구현
     override fun onOptionsItemSelected(item: MenuItem): Boolean {
         return when (item.itemId) {
             android.R.id.home -> {
-                val intent = Intent()
-                intent.putExtra("chatUpdated", true) // 결과 값으로 '갱신 필요' 플래그 전달
-                setResult(Activity.RESULT_OK, intent)
-                Log.d("ChatActivity", "setResult 호출됨") // 로그 추가
                 onBackPressed()
                 true
             }
-
             else -> super.onOptionsItemSelected(item)
         }
-
     }
 }
