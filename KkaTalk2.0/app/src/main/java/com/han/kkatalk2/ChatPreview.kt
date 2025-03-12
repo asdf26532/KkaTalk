c8e4e6587328a656e31ef604824262998da10921
SeungHwan Lee <asdf26532@gmail.com>
2025-03-12 19:57:51 +0900

    chatlist,chatpreview

---
+++ KkaTalk2.0/app/src/main/java/com/han/kkatalk2/ChatPreview.kt
@@ -0,0 +1,15 @@
+package com.han.kkatalk2
+
+data class ChatPreview(
+    val userName: String,
+    val userNick: String,
+    val userUid: String,
+    var lastMessage: String?,
+    val lastMessageTime: Long,
+    val profileImageUrl: String? = null,
+    val unreadCount: Int
+)
+{
+    constructor():this("","","","",0L,"",0)
+}
+
