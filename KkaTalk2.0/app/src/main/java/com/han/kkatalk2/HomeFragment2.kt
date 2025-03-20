e61774f0ddc9d997faf1c69a35e5d36dd80e0f93
SeungHwan Lee <asdf26532@gmail.com>
2025-03-20 22:24:26 +0900

    guide test

---
+++ KkaTalk2.0/app/src/main/java/com/han/kkatalk2/HomeFragment2.kt
@@ -0,0 +1,14 @@
+package com.han.kkatalk2
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+
+class HomeFragment2 : Fragment() {
+
+
+
+
+}
