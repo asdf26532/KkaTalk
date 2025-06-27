4d4370a97908e6dd422d07ffc455091474c3a084
SeungHwan Lee <asdf26532@gmail.com>
2025-06-27 17:22:18 +0900

    admin page

---
+++ KkaTalk2.0/app/src/main/java/com/han/kkatalk2/AdminActivity.kt
@@ -0,0 +1,47 @@
+package com.han.kkatalk2
+
+import android.content.Intent
+import android.os.Bundle
+import android.view.MenuItem
+import android.widget.Button
+import androidx.appcompat.app.AppCompatActivity
+
+class AdminActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_admin)
+
+        supportActionBar?.title = "관리자 대시보드"
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+
+        findViewById<Button>(R.id.btnReportManagement).setOnClickListener {
+            startActivity(Intent(this, ReportManagementActivity::class.java))
+        }
+
+        /*findViewById<Button>(R.id.btnUserManagement).setOnClickListener {
+            startActivity(Intent(this, UserManagementActivity::class.java))
+        }
+
+        findViewById<Button>(R.id.btnPostManagement).setOnClickListener {
+            startActivity(Intent(this, PostManagementActivity::class.java))
+        }
+
+        findViewById<Button>(R.id.btnBlockedUsers).setOnClickListener {
+            startActivity(Intent(this, BlockedUserActivity::class.java))
+        }
+
+        findViewById<Button>(R.id.btnStatistics).setOnClickListener {
+            startActivity(Intent(this, StatisticsActivity::class.java))
+        }*/
+    }
+
+    // 뒤로가기 눌렀을 때 종료
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        if (item.itemId == android.R.id.home) {
+            finish()
+            return true
+        }
+        return super.onOptionsItemSelected(item)
+    }
+
+}
---
+++ KkaTalk2.0/app/src/main/java/com/han/kkatalk2/ReportManagementActivity.kt
@@ -0,0 +1,16 @@
+package com.han.kkatalk2
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class ReportManagementActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_report_management)
+
+
+
+
+
+    }
+}
