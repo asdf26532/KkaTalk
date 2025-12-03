6f1cf870caad14efa33e20c3c7ef35f17edf65fc
SeungHwan Lee <asdf26532@gmail.com>
2025-12-03 15:32:48 +0900

    fullscreenImage

---
+++ Reservation/app/src/main/java/com/han/reservation/FullScreenImageActivity.kt
@@ -0,0 +1,31 @@
+package com.han.reservation
+
+import android.content.Context
+import android.content.Intent
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.viewpager2.widget.ViewPager2
+
+class FullScreenImageActivity : AppCompatActivity() {
+
+    companion object {
+        fun newIntent(context: Context, imageUrls: List<String>, position: Int): Intent {
+            return Intent(context, FullScreenImageActivity::class.java).apply {
+                putStringArrayListExtra("IMAGE_URLS", ArrayList(imageUrls))
+                putExtra("START_POSITION", position)
+            }
+        }
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_full_screen_image)
+
+        val imageUrls = intent.getStringArrayListExtra("IMAGE_URLS") ?: arrayListOf()
+        val startPosition = intent.getIntExtra("START_POSITION", 0)
+
+        val viewPager = findViewById<ViewPager2>(R.id.full_screen_view_pager)
+        viewPager.adapter = GuideImageAdapter(imageUrls)
+        viewPager.setCurrentItem(startPosition, false)
+    }
+}
